7f62d373a844838e06001e91f24fa5d9c36974d8
juanguz1999 <juanguz1999@outlook.com>
2022-07-26 15:02:03 -0500

    Avance 2.2

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import csv
-import math
 
 with open('cargacad.csv') as csv_pca:
     reader = csv.reader(csv_pca, delimiter=";")
@@ -7,11 +6,26 @@ with open('cargacad.csv') as csv_pca:
 
         id_asignatura = row[3]
         aula = row[7]
-        hrdeci = row[9]
-        prueba = type(float(hrdeci))
-        prueba2 = float(hrdeci)*24
-        horas = int(prueba2)
-        Hora_inicio = str(horas) + ':'
+        hrdeci = float(row[9])
+        duracion = int(row[10])
+        tipodato = type(hrdeci)
+        #Hora de inicio
+        Conversion = hrdeci * 24
+        horas = int(Conversion)
+        minutos = int((Conversion * 60) % 60)
+        if minutos == 0:
+            minutos = "00"
+        Hora_inicio = str(horas) + ':' + str(minutos)
+        #Hora Final
+        ConversionFinalp1 = hrdeci+(0.03125*duracion)
+        ConversionFinalp2 = ConversionFinalp1 * 24
+        horasf = int(ConversionFinalp2)
+        minutosf = int((ConversionFinalp2 * 60) % 60)
+        if minutosf == 0:
+            minutosf = "00"
+        Hora_fin = str(horasf) + ':' + str(minutosf)
+
+        #Hora_inicio2 = str(int(math.floor(Conversion))) + ':' + str(int((Conversion % (math.floor(Conversion))) * 60)) + ':' + str(int(((Conversion % (math.floor(Conversion))) * 60) % math.floor(((Conversion % (math.floor(Conversion))) * 60)) * 60))
         #Hora_inicio = str(int(math.floor(prueba2))) + ':' + str(int((prueba2%(math.floor(prueba2)))*60)) + ':' + str(int(((prueba2%(math.floor(prueba2)))*60) % math.floor(((prueba2%(math.floor(prueba2)))*60))*60))
         Json_GCF = {
             'Llave_evento': {
@@ -26,8 +40,8 @@ with open('cargacad.csv') as csv_pca:
                 'nombre_evento': 'Prueba Final',
                 'asistentes': 'pruebas1@pca.edu.co,juan.guzmanm@pca.edu.co',
                 'lugar': aula,
-                #'fecha_inicio': 'Julio 23, 2022 '+Hora_inicio+' PM',
-                'fecha_fin': "Julio 23, 2022 05:00:00 PM",
+                'fecha_inicio': 'Julio 23, 2022 '+Hora_inicio+':00 PM',
+                'fecha_fin': 'Julio 23, 2022 '+Hora_fin+':00 PM',
                 'descripcion': 'HOLA',
                 'recurrencia': 'Semanal',
                 'fecha_fin_evento': 'Julio 31, 2022 05:00:00 PM',
@@ -35,4 +49,4 @@ with open('cargacad.csv') as csv_pca:
                 'idEvent': ''
             }
         }
-        print(Hora_inicio)
+        print(Json_GCF)
